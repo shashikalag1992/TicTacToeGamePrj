99056741150346ab1f2197f436e197a27e30ffb7
Shashi <LENOVO@DESKTOP-2MREFRI>
2023-07-24 16:17:07 +0530

    Add: Check If the dash board field is occupied.

--- src/main/java/com/ttt/tdd/TicTacToe.java
+++ src/main/java/com/ttt/tdd/TicTacToe.java
@@ -1,7 +1,17 @@
 package com.ttt.tdd;
 
+import com.ttt.exceptions.TicTacToeException;
+
 public class TicTacToe {
 
 	private Character[][] board = { { '\0', '\0', '\0' }, { '\0', '\0', '\0' }, { '\0', '\0', '\0' } };
 
+	public void play(int column, int row) {
+
+		if (board[column - 1][row - 1] != '\0') {
+			throw new TicTacToeException("Field is occupied!");
+		} else {
+			board[column - 1][row - 1] = 'X';
+		}
+	}
 }
--- src/test/java/com/ttt/tdd/TicTacToeTest.java
+++ src/test/java/com/ttt/tdd/TicTacToeTest.java
@@ -1,16 +1,37 @@
 package com.ttt.tdd;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
+
+import com.ttt.exceptions.TicTacToeException;
 
 public class TicTacToeTest {
 
 	TicTacToe ticTacToe = new TicTacToe();
 
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
 	@Test
 	public void initializeNewTicTacToeGame() {
 		assertNotNull(ticTacToe);
+	}
+
+	@Test
+	public void checkIfDashBoardOccupied() {
+		ticTacToe.play(2, 1);
+		exception.expect(RuntimeException.class);
+		Exception exception = assertThrows(TicTacToeException.class, () -> {
+			ticTacToe.play(2, 1);
+		});
+		String expectedMessage = "Field is occupied!";
+		String actualMessage = exception.getMessage();
+		assertTrue(actualMessage.contains(expectedMessage));
 	}
 
 }
